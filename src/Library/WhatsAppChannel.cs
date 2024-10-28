c54aeef7a014787ce20e6bdeb3000cca184f3927
Ale Hernandez <alehernandez@gmail.com>
2024-10-28 14:58:05 -0300

    build andando

--- src/Library/WhatsAppChannel.cs
+++ src/Library/WhatsAppChannel.cs
@@ -6,6 +6,7 @@ public class WhatsAppChannel : IMessageChannel
 {
     public void Send(Message message)
     {
-
+        var whatsApp = new WhatsAppApi();
+        whatsApp.Send("+598<Poné tu teléfono acá>", "Hey! I'm using WhatsApp");
     }
 }
