9619320db704a79640611312c13b265ef9a5b36a
Ale Hernandez <alehernandez@gmail.com>
2024-10-29 19:38:47 -0300

    codigo funcionando (sin comentar)

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,11 +8,11 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            var owner = new Contact("Ale", "+598092989394","Alehernandezprincipal@gmail.com");
+            var owner = new Contact("Owner_Ale", "+59892989394","Alehernandezprincipal@gmail.com");
             var phonebook = new Phonebook(owner);
             var messageService = new MessageService(phonebook);
 
-            var contact1 = new Contact("Ale", "+598092989394", "Alehernandezprincipal@gmail.com");
+            var contact1 = new Contact("Ale", "+59892989394", "Alehernandezprincipal@gmail.com");
             phonebook.AddContact(contact1);
 
             var whatsappChannel = new WhatsAppChannel();
